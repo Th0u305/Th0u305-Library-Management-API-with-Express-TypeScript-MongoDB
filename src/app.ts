a2e07c843aaf022168848afb41c6b45ff1bb7122
Th0u305 <ahnaf4411@gmail.com>
2025-07-04 19:50:04 +0600

    cors origin

--- src/app.ts
+++ src/app.ts
@@ -2,8 +2,17 @@
 import express , {Application , Request , Response } from 'express'
 import bookRoutes from './app/routes/bookRoutes'
 import borrowRouter from './app/routes/borrowRoutes'
+import cors from 'cors';
 
 const app : Application = express()
+
+app.use(
+  cors({
+    origin: ["http://localhost:5173", `${process.env.URL__00}`],
+    credentials: true,
+    optionsSuccessStatus: 200,
+  })
+);
 
 app.use(express.json())
 

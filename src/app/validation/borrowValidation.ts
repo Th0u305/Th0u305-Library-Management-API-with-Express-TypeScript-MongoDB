58f925021fc07755f3247fe04fbf1c146451be2f
Th0u305 <ahnaf4411@gmail.com>
2025-07-05 20:18:08 +0600

    borrow dueData string

--- src/app/validation/borrowValidation.ts
+++ src/app/validation/borrowValidation.ts
@@ -3,5 +3,5 @@ import { z } from "zod"
 export const createBorrowSchema = z.object({
   book: z.string().min(1, "Book ID is required"),
   quantity: z.number().min(1, "Quantity must be a positive number"),
-  dueDate: z.string().transform((str) => new Date(str))
+  dueDate: z.string()
 })
